f298f0548ce6f1643f5ded9ca1cf1f29987a92e3
achingachris <achinga.chris@gmail.com>
2022-03-08 10:22:10 +0300

    video-player-component update

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,66 @@
-import React from 'react'
-import { AdvancedVideo } from '@cloudinary/react'
-import { Cloudinary, CloudinaryVideo } from '@cloudinary/url-gen'
-import { preview } from '@cloudinary/url-gen/actions/videoEdit'
-
 const Home = () => {
-  // video preview on hover
-  // onMouseOver
-  const startPreview = (e) => {
+  const media_urls = [
+    {
+      id: 1,
+      title: 'Video One',
+      video_url:
+        'https://res.cloudinary.com/chris101/video/upload/v1645684691/video_1.mp4',
+    },
+    {
+      id: 2,
+      title: 'Video Two',
+      video_url:
+        'https://res.cloudinary.com/chris101/video/upload/v1645684672/video_2.mp4',
+    },
+    {
+      id: 3,
+      title: 'Video Three',
+      video_url:
+        'https://res.cloudinary.com/chris101/video/upload/v1645684663/video_3.mp4',
+    },
+  ]
+
+  // handle mouse enter
+  const handleMouseEnter = (e) => {
     const vid = e.target
     vid.muted = true
     vid.play()
   }
 
-  //  onMouseOut
-  const stopPreview = (e) => {
+  // handle mouse leave
+  const handleMouseLeave = (e) => {
     const vid = e.target
     vid.muted = false
     vid.currentTime = 0
     vid.pause()
   }
 
-  // video urls
-  const video_a =
-    'https://res.cloudinary.com/chris101/video/upload/v1645684691/video_1.mp4'
-  const video_b =
-    'https://res.cloudinary.com/chris101/video/upload/v1645684672/video_2.mp4'
-  const video_c =
-    'https://res.cloudinary.com/chris101/video/upload/v1645684663/video_3.mp4'
-
   return (
-    <div className='container'>
+    <>
       <h1 className='text-center'>Cloudinary Video Show</h1>
       <div className='container'>
         <div className='row'>
-          <div className='col-lg-4 col-md-4'>
-            <div className='container'>
-              <video
-                src={video_a}
-                className='container'
-                controls
-                onMouseEnter={startPreview}
-                onMouseLeave={stopPreview}
-              ></video>
+          {media_urls.map((media) => (
+            <div key={media.id} className='col-lg-4 col-sm-6 mb-4'>
+              <div className='card h-100'>
+                <div className='card-body'>
+                  <h4 className='card-title'>{media.title}</h4>
+                  <video
+                    width='320'
+                    height='240'
+                    controls
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <source src={media.video_url} type='video/mp4' />
+                  </video>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className='col-lg-4 col-md-4'>
-            <div className='container'>
-              <video
-                src={video_b}
-                className='container'
-                controls
-                onMouseEnter={startPreview}
-                onMouseLeave={stopPreview}
-              ></video>
-            </div>
-          </div>
-          <div className='col-lg-4 col-md-4'>
-            <div className='container'>
-              <video
-                src={video_c}
-                className='container'
-                controls
-                onMouseEnter={startPreview}
-                onMouseLeave={stopPreview}
-              ></video>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
